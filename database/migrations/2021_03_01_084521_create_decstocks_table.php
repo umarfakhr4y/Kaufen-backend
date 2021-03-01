4730bbc405e316d1f6aa447c9050d92fa71c2b77
damarardian <damar.rizki212@gmail.com>
2021-03-01 16:40:20 +0700

    upload

---
+++ database/migrations/2021_03_01_084521_create_decstocks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDecstocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decstocks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("barang_id")->unsigned();
+            $table->integer("dec_stock");
+            $table->timestamps();
+            $table->foreign('barang_id')
+            ->references('id')
+            ->on('barangs')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decstocks');
+    }
+}
